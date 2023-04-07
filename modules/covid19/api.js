a11bd9c9390fe01cacc516741c641a09a97e9688
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-04-07 14:10:47 -0400

    reword countries > regions & provinces > areas

--- modules/covid19/api.js
+++ modules/covid19/api.js
@@ -2,16 +2,16 @@ const covid19 = "https://covid-api.com/api";
 
 /* Functions for COVID API Requests */
 
-async function getCountries() {
-  let reqUrl = `${covid19}/regions`; //URL for getting all regions API has data on
+async function getRegions() {
+  let reqUrl = `${covid19}/reports`; //URL for getting all regions API has data on
   let res = await fetch(reqUrl, {
     method: "GET",
   });
   return await res.json();
 }
 
-async function getProvinces(regionISO) {
-  let reqUrl = `${covid19}/provinces/${regionISO}`; // URL for getting all province of a specific region
+async function getAreas(regionIso) {
+  let reqUrl = `${covid19}/reports?iso=${regionIso}`; // URL for getting all province of a specific region
   let res = await fetch(reqUrl, {
     method: "GET",
   });
@@ -19,7 +19,11 @@ async function getProvinces(regionISO) {
 }
 
 async function getReport(regionIso, province) {
-  let reqUrl = `${covid19}/reports?iso=${regionIso}&region_province=${province}`; // URL for getting the report for the specific province
+  let reqUrl = `${covid19}/reports?iso=${regionIso}`;
+  //if the province value is empty, which it can be, then it will return a report for the regionIso ONLY, i.e. this happens with South Korea with no other breakdown of data for any specific areas
+  if (province) {
+    reqUrl += `&region_province=${province}`;
+  }
   let res = await fetch(reqUrl, {
     method: "GET",
   });
@@ -27,7 +31,7 @@ async function getReport(regionIso, province) {
 }
 
 module.exports = {
-  getCountries,
-  getProvinces,
+  getRegions,
+  getAreas,
   getReport,
 };
