11e8b086cc32277cc085f63575862e85f10c181b
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-04-07 17:14:21 -0400

    Add logs to check functionality of getReport()

--- modules/covid19/api.js
+++ modules/covid19/api.js
@@ -12,6 +12,7 @@ async function getRegions() {
 
 async function getAreas(regionIso) {
   let reqUrl = `${covid19}/reports?iso=${regionIso}`; // URL for getting all province of a specific region
+  console.log(`getAreas Region ISO: ${regionIso}`);
   let res = await fetch(reqUrl, {
     method: "GET",
   });
@@ -19,14 +20,26 @@ async function getAreas(regionIso) {
 }
 
 async function getReport(regionIso, areas) {
+  console.log(`getReport Region ISO: ${regionIso}`);
+  console.log(`Province: ${areas}`);
   let reqUrl = `${covid19}/reports?iso=${regionIso}`;
+  console.log(`Pre-if-statement Province: ${areas}`);
   //if the province value is empty, which it can be, then it will return a report for the regionIso ONLY, i.e. this happens with South Korea with no other breakdown of data for any specific areas | also works for if the user wants a report for the whole region
-  if (areas && areas !== "") {
+  console.log("Check - code reached right before if-statement");
+  console.log(`Right before the if statement: ${reqUrl}`);
+  if (areas) {
     reqUrl += `&region_province=${areas}`;
+    console.log(`if-statement triggered, Province: ${areas}`);
+    console.log(
+      `Because the value of province is: ${area}, reqUrl is now: ${reqUrl}`
+    );
   }
+  console.log(`After if statement, was it triggered? reqUrl is: ${reqUrl}`);
   let res = await fetch(reqUrl, {
     method: "GET",
   });
+  console.log(`After await fetch. res: ${res}`);
+
   return await res.json();
 }
 
